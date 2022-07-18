18777e60e290e46a6bf190664e0fa037d2e139b0
ASBAI Halima <83011597+ASBAI-Halima@users.noreply.github.com>
2022-07-18 15:21:11 +0200

    cccc

---
+++ view/Pages/Accueil/accueil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compte Admin</title>
+    <!-- ----------- la biblio chartjs ---------- -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <!-- material icon  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp"
+      rel="stylesheet">
+      <!-- STYLESHEET -->
+
+</head>
+<body>
+</body>
+</html>
---
+++ view/Pages/SignIn-SignUp/signIn.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignIn</title>
+    <!-- ----------- la biblio chartjs ---------- -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <!-- material icon  -->
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp"
+      rel="stylesheet">
+      <!-- STYLESHEET -->
+    <link rel="stylesheet" href="../../Css/SignIn.css">
+</head>
+<body>
+    <div class="header_top">
+        <div class="container">
+            <a href="#" class="logo"><img src="../../img/logoJemeTech.png" alt="logo"></a>
+            <nav class="main_nav">
+                 <ul>
+                    <li><button class="telephone"> <span class="material-icons-sharp">phone</span>Appelez nous 0808526877</button></li>
+                    <li><button class="sign-up-btn">Connexion</button></li>
+                    <li> <button class="sign-up-btn">Inscription</button></li>
+                    <li>  <button id="menu-btn">
+                    <span class="material-icons-sharp">menu</span>
+                    </button></li>
+
+
+
+                 </ul>
+               
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
